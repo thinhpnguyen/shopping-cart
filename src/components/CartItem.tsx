881f99fc81843462ad19bf5659b8df1bfd863651
Thinh Nguyen <thinhnguyen@Thinhs-MacBook-Pro.local>
2022-09-19 22:10:10 -0700

    done with shopping cart sidebar

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
 import storeItems from "../data/items.json";
 import { useShoppingCart } from "../context/ShoppingCartContext";
-import { Stack } from "react-bootstrap";
+import { Stack, Button } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
 type CartItemProps = {
   id: number;
   quantity: number;
@@ -8,7 +9,7 @@ type CartItemProps = {
 
 export function CartItem({ id, quantity }: CartItemProps) {
   const item = storeItems.find((item) => item.id === id);
-  const removeFromCart = useShoppingCart();
+  const { removeFromCart } = useShoppingCart();
   if (item == null) return null;
 
   return (
@@ -17,6 +18,28 @@ export function CartItem({ id, quantity }: CartItemProps) {
         src={item.imgUrl}
         style={{ width: "125px", height: "75px", objectFit: "cover" }}
       />
+
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+      <div>{formatCurrency(item.price * quantity)}</div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </Button>
     </Stack>
   );
 }
